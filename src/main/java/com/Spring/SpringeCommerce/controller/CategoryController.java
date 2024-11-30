27b1ebe9a7c18c7083c36ab9d92eb183d80a8811
Weslley Nascimento <wesndsantos@gmail.com>
2024-11-30 07:05:44 -0400

    :sparkles: Category controller

---
+++ src/main/java/com/Spring/SpringeCommerce/controller/CategoryController.java
@@ -0,0 +1,99 @@
+package com.Spring.SpringeCommerce.controller;
+
+import com.Spring.SpringeCommerce.exceptions.AlreadyExistsException;
+import com.Spring.SpringeCommerce.exceptions.ResourceNotFoundException;
+import com.Spring.SpringeCommerce.interfaces.ICategoryService;
+import com.Spring.SpringeCommerce.model.Category;
+import com.Spring.SpringeCommerce.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/categories")
+public class CategoryController {
+
+    @Autowired
+    private ICategoryService categoryService;
+
+    @GetMapping("/all")
+    public ResponseEntity<ApiResponse> getAllCategories(){
+        try {
+            List<Category> categories = categoryService.getAllCategories();
+
+            return ResponseEntity.ok(new ApiResponse("Found", categories));
+        } catch (Exception e){
+            return ResponseEntity
+                    .status(INTERNAL_SERVER_ERROR)
+                    .body(new ApiResponse("Error:", INTERNAL_SERVER_ERROR));
+        }
+    }
+
+    @PostMapping("/new-category")
+    public ResponseEntity<ApiResponse> addCategory(@RequestBody Category categoryName){
+        try {
+            Category category = categoryService.addCategory(categoryName);
+            return ResponseEntity.ok(new ApiResponse("Success", category));
+        } catch (AlreadyExistsException e){
+            return ResponseEntity
+                    .status(CONFLICT)
+                    .body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @GetMapping("/category/${categoryId}")
+    public ResponseEntity<ApiResponse> getCategoryById(@PathVariable Long id){
+        try {
+            Category category = categoryService.getCategoryById(id);
+            return ResponseEntity.ok(new ApiResponse("Found", category));
+        } catch (ResourceNotFoundException e){
+            return ResponseEntity
+                    .status(NOT_FOUND)
+                    .body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @GetMapping("/category/${categoryName}")
+    public ResponseEntity<ApiResponse> getCategoryByName(@PathVariable String categoryName){
+        try {
+            Category category = categoryService.getCategoryByName(categoryName);
+            return ResponseEntity.ok(new ApiResponse("Found", category));
+        } catch (ResourceNotFoundException e){
+            return ResponseEntity
+                    .status(NOT_FOUND)
+                    .body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @DeleteMapping("/{categoryId}/delete")
+    public ResponseEntity<ApiResponse> deleteCategory(@PathVariable Long id){
+        try {
+            categoryService.deleteCategory(id);
+            return ResponseEntity.ok(new ApiResponse("Found", null));
+        } catch (ResourceNotFoundException e){
+            return ResponseEntity
+                    .status(NOT_FOUND)
+                    .body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+
+    @PutMapping("/category/${categoryId}/update")
+    public ResponseEntity<ApiResponse> updateCategory(@PathVariable Long id, @RequestBody Category category){
+        try {
+            Category updatedCategory = categoryService.updateCategory(category, id);
+
+            return ResponseEntity.ok(new ApiResponse("Update successful!", category));
+        } catch (ResourceNotFoundException e) {
+            return ResponseEntity
+                    .status(NOT_FOUND)
+                    .body(new ApiResponse(e.getMessage(), null));
+        }
+    }
+}
